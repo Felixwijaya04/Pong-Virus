825ac7a0a49fb7498a398560238b220e81738235
Felixwijaya04 <felixwijaya2004@gmail.com>
2024-03-25 10:42:24 +0700

    unfixed bug on goal system

--- PONG2D/Assets/leftgoal.cs
+++ PONG2D/Assets/leftgoal.cs
@@ -6,9 +6,19 @@ using UnityEngine.UI;
 public class leftgoal : MonoBehaviour
 {
     public scoreManager sm;
+    public GameObject ball;
+    Vector2 ballPos;
 
+    private void Start()
+    {
+        ballPos = ball.transform.position;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         sm.leftWin(1);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ball.transform.position = ballPos;
     }
 }
--- PONG2D/Assets/rightGoal.cs
+++ PONG2D/Assets/rightGoal.cs
@@ -6,9 +6,18 @@ using UnityEngine.UI;
 public class rightGoal : MonoBehaviour
 {
     public scoreManager sm;
-
+    public GameObject ball;
+    Vector2 ballPos;
+    private void Start()
+    {
+        ballPos = ball.transform.position;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         sm.rightWin(1);
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ball.transform.position = ballPos;
     }
 }
